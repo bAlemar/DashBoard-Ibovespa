23b063a104b0fed3fa0d89f49f7b8bd10c8018f4
Bernardo Alemar <bernardo.alemar@hotmail.com>
2024-01-08 20:09:57 -0300

    Scraping para ambos dash

--- scraping.py
+++ scraping.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import regex as re
 
-def scraping_noticia(stock_name,qntd_noticias = 3):
+
+
+def scraping_titulo_noticia(stock_name,qntd_noticias = 3):
     #Request
     url = f'https://braziljournal.com/?s={stock_name}'
 
@@ -14,7 +17,7 @@ def scraping_noticia(stock_name,qntd_noticias = 3):
     todas_noticias = soup.find_all(class_='wrap-bj')
 
     #Dicionário para armazenar as informações:
-    dict_url = {
+    dict_scraping = {
         'titulo' : [],
         'link' : [],
         'categoria' : []
@@ -31,7 +34,37 @@ def scraping_noticia(stock_name,qntd_noticias = 3):
     
         # Looping com os elementos para pegar informações de cada noticia
         for elementos_titulo, elemento_categoria in zip(elementos_titulo, elemento_categoria):
-            dict_url['titulo'].append(elementos_titulo.a.text.strip())
-            dict_url['link'].append(elementos_titulo.a['href'])
-            dict_url['categoria'].append(elemento_categoria.a.text.strip())
-    return dict_url
+            dict_scraping['titulo'].append(elementos_titulo.a.text.strip())
+            dict_scraping['link'].append(elementos_titulo.a['href'])
+            dict_scraping['categoria'].append(elemento_categoria.a.text.strip())
+    return dict_scraping
+
+def scraping_texto_noticia(link):
+    # Há notícias com link /play/ antes que possui class que contém texto diferente
+    # Para isso foi criado condições caso contenha /play/ na url ou não.
+    
+    #Request
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text,'html.parser')
+    
+    # Palavra chave 
+    key_word = r'/play/'    
+    resultado = re.search(key_word,link)
+    
+    #Variável que armazenará o texto
+    texto_full = ''
+    
+    #Condição de link
+    if resultado:
+        noticia = soup.find_all(class_='boxarticle-infos-text')[0]
+        paragrafos = noticia.find_all('p')
+
+    else:
+        noticia = soup.find_all(class_='post-content-text')[0]
+        paragrafos = noticia.find_all('p')
+    
+    #Juntando todos os paragrafos da noticia
+    for parag in paragrafos:
+        texto_full += parag.text
+    
+    return texto_full
